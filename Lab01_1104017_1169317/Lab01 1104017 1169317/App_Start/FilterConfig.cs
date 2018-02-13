62f59496580198b7a94c1e6e9f73b3e3a05beccb
paas10 <pablo.andre_alvarado@hotmail.com>
2018-02-13 11:29:17 -0600

    Primer commit

---
+++ Lab01_1104017_1169317/Lab01 1104017 1169317/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01_1104017_1169317
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
