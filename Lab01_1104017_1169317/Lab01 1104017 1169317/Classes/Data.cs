932f7bd89185bee52e2e1897169ca052599f25db
paas10 <pablo.andre_alvarado@hotmail.com>
2018-02-16 10:16:11 -0600

    Insertar

---
+++ Lab01_1104017_1169317/Lab01 1104017 1169317/Classes/Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab01_1104017_1169317.Classes
+{
+    public class Data
+    {
+        private static Data instance;
+        public static Data Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Data();
+                return instance;
+            }
+        }
+
+        public List<Jugador> JugadoresCSharp;
+
+        public Data()
+        {
+            JugadoresCSharp = new List<Jugador>();
+        }
+    }
+}
